b290d0f4902cb980d3ff960ba70702d16a638c30
riya09 <faria.ayir@gmail.com>
2024-02-02 23:58:01 +0600

    feat: add basic component of Leaf

---
+++ src/components/Tree/Leaf.jsx
@@ -0,0 +1,55 @@
+import { useState, useEffect, useRef } from "react"
+import { CSSTransition } from "react-transition-group"
+
+const Leaf = ({ index, visible }) => {
+  const [isEntered, setIsEntered] = useState(false)
+  const nodeRef = useRef(null)
+
+  useEffect(() => {
+    // Trigger the animation after the initial render
+    setIsEntered(true)
+  }, [])
+
+  const setLeafPosition = (index, direction) => {
+    const isLeft = direction === 'left'
+    const initialPosition = {
+      y: 0,
+      deg: isLeft ? 90 : 15,
+    }
+    const offset = 24
+    const yPosition = initialPosition.y + (index * offset)
+    const transform = `translate(0, ${-yPosition}px) rotate(${initialPosition.deg}deg)`
+    return transform
+  }
+  return (
+    <CSSTransition
+      in={isEntered && visible}
+      nodeRef={nodeRef}
+      timeout={30}
+      classNames="item"
+    >
+      {(state) => (
+        <div ref={nodeRef}>
+          <div
+            style={{
+              transform: `${setLeafPosition(index, "left")} scale(${
+                state === "entered" ? 1 : 0
+              })`,
+            }}
+            className="leaf left"
+          />
+          <div
+            style={{
+              transform: `${setLeafPosition(index, "right")} scale(${
+                state === "entered" ? 1 : 0
+              })`,
+            }}
+            className="leaf right"
+          />
+        </div>
+      )}
+    </CSSTransition>
+  );
+}
+
+export default Leaf
