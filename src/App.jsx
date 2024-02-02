d7a2cf316778be84135c8f389983d0ee045ef824
riya09 <faria.ayir@gmail.com>
2024-02-02 23:58:41 +0600

    feat: wrapped all elements in app component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
+import { useState } from 'react'
+import './App.css'
+import Slider from "./components/Slider"
+import Tree from './components/Tree'
+
 function App() {
+  const [year, setYear] = useState(0)
+  const [yearIndex, setYearIndex] = useState(0)
+  const getValueFromSlider = (sliderYear, sliderYearIndex) => {
+    setYear(sliderYear)
+    setYearIndex(Number(sliderYearIndex))
+  } 
   return (
     <>
       <p>Tree animation</p>
+      <Tree year={year} yearIndex={yearIndex} />
+      <Slider getVal={(y, i) => getValueFromSlider(y, i)} />
     </>
-  )
+  );
 }
 
 export default App
