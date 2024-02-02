de9d8e03316cdd7ed883e521489e2efe81b61077
riya09 <faria.ayir@gmail.com>
2024-02-02 23:58:19 +0600

    feat: add tree component and import leaves

---
+++ src/components/Tree/index.jsx
@@ -0,0 +1,36 @@
+import { useEffect,useState, useRef } from 'react'
+import './index.scss'
+import Leaf from './Leaf';
+import { TransitionGroup } from 'react-transition-group';
+
+const Tree = ({ year, yearIndex }) => {
+  const createArray = (n) => Array.from({ length: n + 1 });
+  const [leaves, setLeaves] = useState(createArray(yearIndex))
+  const treePot = useRef(null)
+
+  useEffect(() => {
+    treePot.current.style.height = `${(yearIndex * 25) + 50}px`
+    let timer = setTimeout(() => {
+      setLeaves(createArray(yearIndex));
+    }, leaves.length > yearIndex + 1 ? 1500 : 0)
+    return () => {
+      clearTimeout(timer)
+    }
+  }, [yearIndex, leaves])
+
+
+  return (
+    <div className="tree-wrapper">
+      <div className="pot">
+        <div ref={treePot} className="branch">
+            {leaves.map((_, l) => (
+              <Leaf index={l} key={l} visible={l < yearIndex + 1}/>
+            ))}
+          <div className="leaf top"></div>
+        </div>
+      </div>
+    </div>
+  );
+}
+ 
+export default Tree;
