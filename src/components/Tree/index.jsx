81db29ca341077e08afd4e23c9466fcd85d441b7
riya09 <faria.ayir@gmail.com>
2024-02-06 03:10:24 +0600

    refactor: change pot and leave elements

--- src/components/Tree/index.jsx
+++ src/components/Tree/index.jsx
@@ -3,12 +3,12 @@ import './index.scss'
 import Leaf from './Leaf';
 
 const Tree = ({ year, yearIndex }) => {
-  const createArray = (n) => Array.from({ length: n + 1 });
+  const createArray = (n) => Array.from({ length: n + 1 })
   const [leaves, setLeaves] = useState(createArray(yearIndex))
   const treePot = useRef(null)
 
   useEffect(() => {
-    treePot.current.style.height = `${(yearIndex * 25) + 50}px`
+    treePot.current.style.height = `${(yearIndex * 25) + 45}px`
     let timer = setTimeout(() => {
       setLeaves(createArray(yearIndex));
     }, leaves.length > yearIndex + 1 ? 1500 : 0)
@@ -20,13 +20,26 @@ const Tree = ({ year, yearIndex }) => {
 
   return (
     <div className="tree-wrapper">
-      <div className="pot">
+      <div className="tree">
         <div ref={treePot} className="branch">
-            {leaves.map((_, l) => (
-              <Leaf index={l} key={l} visible={l < yearIndex + 1}/>
-            ))}
+          {leaves.map((_, l) => (
+            <Leaf index={l} key={l} visible={l < yearIndex + 1} />
+          ))}
           <div className="leaf top"></div>
         </div>
+        <svg
+          className="pot"
+          width="70"
+          viewBox="0 0 43 28"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path d="M5.99995 28L3 3H40L37 28H5.99995Z" fill="#AB5446" />
+          <path
+            d="M42.5 5.14815H0.999997C0.999997 1.54815 1.43051e-06 0.148148 5 0.148149H38C43.5 0.148149 42.5 1.64815 42.5 5.14815Z"
+            fill="#863332"
+          />
+        </svg>
       </div>
     </div>
   );
